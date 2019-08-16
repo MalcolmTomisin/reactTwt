0525799a6d48cb16501e25d509356b46d4f14663
Oluwatomisin <malcolmtomisin@gmail.com>
2019-08-16 20:47:06 +0100

    contributing page

---
+++ src/contribute/relax.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Contribute extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            file: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">Upload Image/Video and Provide required Information</div>
+                <div className="box">
+                    <div className="input-group">
+                        <input type="file" className="login-upload" onChange={this.handleChange}/>
+                        <img src={this.state.file}/>
+                    </div>
+                    
+                    <div className="input-group">
+                        <label htmlFor="coordinates" className="login-label">Coordinates</label>
+                        <input type="number" name="coordinates" placeholder="Enter Coordinates" className="login-input" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="bilbord-tag" className="login-label">Bilbord Tag</label>
+                        <input type="text" name="bilbord-tag" placeholder="Enter Bilbord Tag" className="login-input" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="bilbord-pin" className="login-label">Bilbord Pin</label>
+                        <input type="text" name="bilbord-pin" placeholder="Enter the Bilbord Pin" className="login-input" />
+                    </div>
+
+                    <button type="button" className="login-btn" onClick={}>Submit Bilbord</button>
+                </div>
+            </div>
+        )
+    }
+}
